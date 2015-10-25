c4cac36ee883308f7bb3b797b2c53f11b7ff6108
prokhor-ozornin <prokhor.ozornin@yandex.ru>
2015-10-25 19:16:29 +0500

    Version 2.0
    1. Changed signature of IMobileDetector to return Task<IMobileDevice> instead of IMobileDevice, switching to async architecture.
    
    2. Altered list of supported .NET runtimes to :
    - .NET 4.5
    - .NET 4.6
    - Windows 8.1
    - Windows Phone 8.1
    - Windows Universal Platform
    
    3. Changed project structure - moved all code to separate Yandex.Detector directory and linked it to all solution's projects.
    
    4. Updated used version of Catharsis.Commons package to 3.0.2.

---
+++ src/Yandex.Detector/IMobileDetector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Yandex.Detector
+{
+  /// <summary>
+  ///   <para>Provides access to functionality of Yandex.Detector mobile service.</para>
+  /// </summary>
+  public interface IMobileDetector
+  {
+    /// <summary>
+    ///   <para>Performs request to Yandex.Detector web service and determines capabilities of mobile client device, using provided HTTP headers for its identification.</para>
+    /// </summary>
+    /// <param name="headers">Map of mobile device's HTTP headers with values.</param>
+    /// <returns>Instance of <see cref="IMobileDevice"/> object, describing capabilities of identified mobile device.</returns>
+    /// <exception cref="ArgumentNullException">If <paramref name="headers"/> is a <c>null</c> reference.</exception>
+    /// <exception cref="DetectorException">If there was error either during the request to Yandex.Detector web service, or mobile device cannot be identified based on a set of provided HTTP headers.</exception>
+    Task<IMobileDevice> Detect(IDictionary<string, object> headers);
+  }
+}
---
+++ src/Yandex.Detector/MobileDetector.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Catharsis.Commons;
+
+namespace Yandex.Detector
+{
+  internal sealed class MobileDetector : IMobileDetector
+  {
+    private const string EndpointUrl = "http://phd.yandex.net/detect/";
+
+    public async Task<IMobileDevice> Detect(IDictionary<string, object> headers)
+    {
+      Assertion.NotNull(headers);
+
+      if (!headers.Any())
+      {
+        throw new DetectorException("No HTTP headers were specified");
+      }
+
+      var response = string.Empty;
+
+#if NET_40
+      var client = new WebClient();
+      foreach (var header in headers)
+      {
+        client.Headers[header.Key] = header.Value?.ToStringInvariant();
+      }
+
+      try
+      {
+        response = client.DownloadString(EndpointUrl);
+      }
+      catch (Exception exception)
+      {
+        throw new DetectorException("Network communication error", exception);
+      }
+#else
+      using (var client = new System.Net.Http.HttpClient())
+      {
+        foreach (var header in headers)
+        {
+          client.DefaultRequestHeaders.Add(header.Key, header.Value?.ToStringInvariant());
+          try
+          {
+            response = await client.GetStringAsync(EndpointUrl);
+          }
+          catch (Exception exception)
+          {
+            throw new DetectorException("Network communication error", exception);
+          }
+        }
+      }
+#endif
+      try
+      {
+        var error = response.AsXml<Error>();
+        throw new DetectorException(error.Text);
+      }
+      catch (InvalidOperationException)
+      {
+      }
+
+      try
+      {
+        return response.AsXml<MobileDevice>();
+      }
+      catch (Exception exception)
+      {
+        throw new DetectorException("Failed to understand service's response", exception);
+      }
+    }
+  }
+}
